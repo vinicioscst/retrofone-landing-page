21cc0b71330ce07e3e695d4723c81dcf317fdc73
Vinícios Costa <dev.vinicios@gmail.com>
2024-09-11 22:10:17 -0300

    chore: adds sonner toast

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter, Spicy_Rice } from 'next/font/google'
+import { Toaster } from 'sonner'
 
 export const inter = Inter({
   subsets: ['latin'],
@@ -42,6 +43,10 @@ export default function RootLayout({
       className='scroll-smooth'
       lang='pt-BR'
     >
+      <Toaster
+        richColors
+        position='top-right'
+      />
       <body>{children}</body>
     </html>
   )
